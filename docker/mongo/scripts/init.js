10c74da5d15154a0af14c5fcf35741cb3cd9e28d
Ilya Shatov <4i9wh9hrw@mozmail.com>
2022-08-26 18:59:58 +0300

    Update init.js

--- docker/mongo/scripts/init.js
+++ docker/mongo/scripts/init.js
@@ -1,8 +1,8 @@
 rs.status();
 db = db.getSiblingDB("graylog");
 db.createUser({
-    user: 'log',
-    pwd: 'log',
+    user: '$DB_USER',
+    pwd: '$DB_PASS',
     roles: [
       {
         role: 'readWrite',
